1e5e0bc623215a4febca402255513c0d35ba0b00
bob bae <bob.bae@gmail.com>
2020-05-21 22:33:48 -0700

    init

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import (Flask,render_template,request,Response,jsonify)
+from newscatcher import Newscatcher
+from newscatcher import urls, describe_url
+import logging
+from logging.handlers import RotatingFileHandler
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+handler = RotatingFileHandler('flask_news.log',maxBytes=10000,backupCount=1)
+handler.setLevel(logging.DEBUG)
+app.logger.addHandler(handler)
+app.logger.info('starting')
+
+@app.route('/')
+def myindex():
+    global lacovidlist
+    app.logger.info("myindex")
+    return render_template("index.html",news=get_news('latimes.com'), next_page=1, prev_page=0)
+
+topics = [ 'tech', 'news', 'business', 'science', 'finance', 
+        'food', 'politics', 'economics', 'travel', 'entertainment', 
+        'music', 'sport', 'world' ]
+
+def get_news(sitename):
+    nc = Newscatcher(website = sitename)
+    results = nc.get_news()
+    articles = results['articles']
+    #nytimes.print_headlines(n=10)
+    #urls_pol = urls(topic = 'politics')
+    #describe_url(urls_pol[1])
+    #atlantic = Newscatcher(website = urls_pol[1], topic = 'politics')    
+    for a in articles:
+        a.source = sitename
+    return articles
+
+if __name__=='__main__':
+    app.run(threaded=True, port=5000)
