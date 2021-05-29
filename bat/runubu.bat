47c02893d12de7c38fba44d008e1d5c9086f9cd7
bob bae <bob.bae@gmail.com>
2021-05-29 11:30:04 -0700

    bat

--- bat/runubu.bat
+++
@@ -1,2 +0,0 @@
-#docker run --name bobuntu --rm --detach-keys=ctrl-@,@ -it -v c:/users/bobba/space:/home/bob/space   registry.gitlab.com/bobbae/bobuntu sudo su bob
-docker run --name ubu1 --rm --detach-keys=ctrl-@,@ -it -v c:/users/bobba/space:/home/bob/space   bobbae/ubu1 bash
