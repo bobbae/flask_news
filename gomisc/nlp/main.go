fba4d5a849f10306f96c64d8f005d943baf17422
bob bae <bob.bae@gmail.com>
2021-07-04 18:02:52 +0000

    flag

--- gomisc/nlp/main.go
+++ gomisc/nlp/main.go
@@ -1,48 +1,52 @@
-
 // Sample language-quickstart uses the Google Cloud Natural API to analyze the
 // sentiment of "Hello, world!".
 package main
 
 import (
-        "context"
-        "fmt"
-        "log"
+	"context"
+	"flag"
+	"fmt"
+	"log"
 
-        language "cloud.google.com/go/language/apiv1"
-        languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
+	language "cloud.google.com/go/language/apiv1"
+	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
 )
 
 func main() {
-        ctx := context.Background()
+	textPtr := flag.String("text", "Hello world", "a sentence")
+	flag.Parse()
 
-        // Creates a client.
-        client, err := language.NewClient(ctx)
-        if err != nil {
-                log.Fatalf("Failed to create client: %v", err)
-        }
-        defer client.Close()
+	ctx := context.Background()
 
-        // Sets the text to analyze.
-        text := "Hello, world!"
+	// Creates a client.
+	client, err := language.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
 
-        // Detects the sentiment of the text.
-        sentiment, err := client.AnalyzeSentiment(ctx, &languagepb.AnalyzeSentimentRequest{
-                Document: &languagepb.Document{
-                        Source: &languagepb.Document_Content{
-                                Content: text,
-                        },
-                        Type: languagepb.Document_PLAIN_TEXT,
-                },
-                EncodingType: languagepb.EncodingType_UTF8,
-        })
-        if err != nil {
-                log.Fatalf("Failed to analyze text: %v", err)
-        }
+	// Sets the text to analyze.
+	text := *textPtr
+	fmt.Println("text: ", text)
 
-        fmt.Printf("Text: %v\n", text)
-        if sentiment.DocumentSentiment.Score >= 0 {
-                fmt.Println("Sentiment: positive")
-        } else {
-                fmt.Println("Sentiment: negative")
-        }
+	// Detects the sentiment of the text.
+	sentiment, err := client.AnalyzeSentiment(ctx, &languagepb.AnalyzeSentimentRequest{
+		Document: &languagepb.Document{
+			Source: &languagepb.Document_Content{
+				Content: text,
+			},
+			Type: languagepb.Document_PLAIN_TEXT,
+		},
+		EncodingType: languagepb.EncodingType_UTF8,
+	})
+	if err != nil {
+		log.Fatalf("Failed to analyze text: %v", err)
+	}
+
+	fmt.Printf("Text: %v Score: %v\n", text, sentiment.DocumentSentiment.Score)
+	if sentiment.DocumentSentiment.Score >= 0 {
+		fmt.Println("Sentiment: positive")
+	} else {
+		fmt.Println("Sentiment: negative")
+	}
 }
